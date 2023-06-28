6a3b6a802ebcc8361b4fba317c25f2f606f937ef
Leslie <Kooliesk@gmail.com>
2023-06-28 23:12:25 +0200

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Login and registration pages
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
@@ -18,20 +17,15 @@ Rails.application.routes.draw do
     end
   end
 
-  # Recipes list routes
-  resources :recipes, except: [:update]
+  resources :recipes do
+    post :toggle, on: :member
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
+  
+  get 'recipes/:id/shopping_list', to: 'foods#shopping_list', as: 'recipe_shopping_list'
 
   # Public recipe list routes
   resources :public_recipes, only: [:index]
-
-  # Recipe details routes
-  resources :recipes, only: [:show] do
-    resources :foods, only: %i[new create], module: :recipes
-  end
-
-  resources :recipes do
-    patch 'toggle_public', on: :member
-  end
 
   # Navigation menu routes
   root to: 'foods#index'
