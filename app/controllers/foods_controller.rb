5940293ac6b71090aeb6bfeb011f957a34e2502a
Leslie <Kooliesk@gmail.com>
2023-06-29 10:23:50 +0200

    fix rubocop error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -34,21 +34,21 @@ class FoodsController < ApplicationController
   def shopping_list
     @recipe = Recipe.find(params[:id])
     return unless @recipe
-  
+
     @recipe_foods = @recipe.recipe_foods.includes(:food)
-  
+
     @shopping_list = @recipe_foods.map do |recipe_food|
       food = recipe_food.food
       quantity = recipe_food.quantity
       value = quantity * food.price
-  
+
       {
-        food: food,
-        quantity: quantity,
-        value: value
+        food:,
+        quantity:,
+        value:
       }
     end
-  
+
     @total_food_items = @shopping_list.length
     @total = @shopping_list.sum { |item| item[:value] }
   end
