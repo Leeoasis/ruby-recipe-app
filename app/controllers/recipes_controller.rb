041f50f438a580b0affa7e294b94341907041c8d
Amen-Musingarimi <takudzwamusinga@gmail.com>
2023-06-29 10:34:34 +0200

    Add Recipe author to public recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -50,6 +50,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :description, :public)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
