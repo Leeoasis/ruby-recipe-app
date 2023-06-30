82a182872f05b679716cc59dea035ce694310060
Leslie Gudo <Kooliesk@gmail.com>
2023-06-30 10:43:07 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
   create_table 'foods', force: :cascade do |t|
     t.string 'name'
     t.string 'measurement_unit'
@@ -24,7 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
     t.datetime 'updated_at', null: false
     t.index ['user_id'], name: 'index_foods_on_user_id'
   end
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
   create_table 'recipe_foods', force: :cascade do |t|
     t.integer 'quantity'
     t.bigint 'recipe_id', null: false
@@ -34,7 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
     t.index ['food_id'], name: 'index_recipe_foods_on_food_id'
     t.index ['recipe_id'], name: 'index_recipe_foods_on_recipe_id'
   end
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
   create_table 'recipes', force: :cascade do |t|
     t.string 'name'
     t.integer 'preparation_time'
@@ -46,7 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
     t.datetime 'updated_at', null: false
     t.index ['user_id'], name: 'index_recipes_on_user_id'
   end
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.datetime 'created_at', null: false
@@ -63,7 +70,9 @@ ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+end
 
+ActiveRecord::Schema[7.0].define(version: 20_230_626_182_642) do
   add_foreign_key 'foods', 'users'
   add_foreign_key 'recipe_foods', 'foods'
   add_foreign_key 'recipe_foods', 'recipes'
